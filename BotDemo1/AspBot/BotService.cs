cf927cfee0f30cf3c934980ca38802eeb762d2d2
Serhii Sudakov <serhiisudakov@gmail.com>
2024-05-06 19:00:36 +0300

    remove console

--- BotDemo1/AspBot/BotService.cs
+++ BotDemo1/AspBot/BotService.cs
@@ -1,13 +1,18 @@
 ﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace AspBot
 {
     public class BotService : BackgroundService
     {
         private readonly TelegramBotClient _botClient;
-
+        ReceiverOptions receiverOptions = new()
+        {
+            AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+        };
         public BotService()
         {
             _botClient = new TelegramBotClient("7182360517:AAFMJseBoasM9m0P0LygoFiz2jX0ufZV5fU");
@@ -18,7 +23,7 @@ namespace AspBot
             _botClient.StartReceiving(
                 HandleUpdateAsync,
                 HandleErrorAsync,
-                new ReceiverOptions(),
+                receiverOptions,
                 stoppingToken
             );
 
@@ -31,16 +36,45 @@ namespace AspBot
 
         private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            if (update.Message?.Text is { } messageText)
+            if (update.Message is not { } message)
+                return;
+            // Only process text messages
+            if (message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+            var userName = message.Chat.Username;
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}. an user Name {userName}");
+
+            switch (messageText)
             {
-                var chatId = update.Message.Chat.Id;
-                await botClient.SendTextMessageAsync(chatId, "Echo: " + messageText, cancellationToken: cancellationToken);
+                case "api":
+                    await botClient.SendTextMessageAsync(chatId, "Ссылка на документацию API бота: https://core.telegram.org/bots/api", cancellationToken: cancellationToken);
+                    break;
+                case "help":
+                    await botClient.SendTextMessageAsync(chatId, "Вы написали умному боту, используйте команды: api, help, привет", cancellationToken: cancellationToken);
+                    break;
+                case "привет":
+                    await botClient.SendTextMessageAsync(chatId, "Привет! Я ваш умный Telegram бот. Я могу помочь вам с информацией по API и другим вопросам.", cancellationToken: cancellationToken);
+                    break;
+                default:
+                    // Отправляем сообщение обратно пользователю
+                    await botClient.SendTextMessageAsync(chatId, "Вы сказали: " + messageText, cancellationToken: cancellationToken);
+                    break;
             }
         }
 
         private Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
-            Console.WriteLine($"Error: {exception.ToString()}");
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
             return Task.CompletedTask;
         }
     }
